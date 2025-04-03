6cac5a139ead2bbfedcad643373f00dd36448383
root <root@DESKTOP-6L52GRJ.>
2025-04-03 18:02:50 +0000

    Removed the errors

--- Day-4-Terraform-Loops-MetaArguments-SplatOperator/Terraform-manifest/c5-ec2instance.tf
+++ Day-4-Terraform-Loops-MetaArguments-SplatOperator/Terraform-manifest/c5-ec2instance.tf
@@ -10,13 +10,19 @@ data "aws_subnet" "default" {
 
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
-  instance_type = var.instance_type
+  # instance_type = var.instance_type_list[1]
+  instance_type = var.instance_type_map["prod"]
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   subnet_id     = data.aws_subnet.default.id 
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+
+  # Meta-Argument Count
+  count = 2
+
+  # count.index
   tags = {
-    "Name" = "EC2 Demo 2"
+    "Name" = "Count-Demo-${count.index}"
   }
 }
